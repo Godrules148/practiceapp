b1106bee0faef80d98585dd33059c2f49e90227d
Jeremy G. Bray <j_bray14@yahoo.com>
2013-11-08 17:39:18 -0600

    Created a new git repository for practiceapp

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,5 @@
+class GreetingsController < ApplicationController
+  def index
+  	@message = "Hello, how are you today?"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Practiceapp::Application.config.secret_token = 'f695bad92887b0ad1fb4f51fa4d617b6633a0f91841649e853f398a03c5ceb510c7fbd3fd531f863cbef62d05e52c0b279a79f7408cec939713e51e1086fc4c1'
